91136177acbf3dc779def06c143bd6673fff0081
Pecheverriar <paulo.echeverria1407@gmail.com>
2024-03-05 00:34:48 -0300

    introduccion a JS

---
+++ Assets/JS/index.js
@@ -0,0 +1,12 @@
+let precio = 400000
+let cantidad = 1
+
+let precioSpan = document.querySelector(".precio-inicial");
+let cantidadSpan = document.querySelector(".cantidad");
+let pagoSpan = document.querySelector(".pago");
+
+
+precioSpan.innerHTML = precio;
+cantidadSpan.innerHTML = cantidad;
+pagoSpan.innerHTML = precio;
+
